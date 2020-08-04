731916e14cf33e7a1ac94512089ad1f71e0a9392
zzzukin <62421661+zzzukin@users.noreply.github.com>
2020-08-04 17:34:03 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,3 @@
-/**
-      * Scans object from for all getters. If object to
-      * contains correspondent setter, it will invoke it
-      * to set property value for to which equals to the property
-      * of from.
-      *
-      * The type in setter should be compatible to the value returned
-      * by getter (if not, no invocation performed).
-      * Compatible means that parameter type in setter should
-      * be the same or be superclass of the return type of the getter.
-      *
-      * The method takes care only about public methods.
-      *
-      * @param to   Object which properties will be set.
-      * @param from Object which properties will be used to get values.
-      */
-
 package com.company;
 
 import java.lang.reflect.InvocationTargetException;
